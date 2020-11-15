509a9f90263d6280072075f68911f5c05cbb4283
Mohammad Mostafa <muhammad.mostafa.33@gmail.com>
2020-11-16 00:14:58 +0200

    register category and product in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug", "price", "availabe", "created", "updated"]
+    list_filter = ["availabe", "created", "updated"]
+    list_editable = ["price", "availabe"]
+    prepopulated_fields = {"slug": ("name",)}
